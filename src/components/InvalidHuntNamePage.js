a2ca4e6ee79415e788088b2dc5ec390df57bb75b
Thobias <thobias.maaoe@gmail.com>
2022-02-23 17:43:21 +0100

    endret teksten inni knappen på InvalidHuntNamePage

--- src/components/InvalidHuntNamePage.js
+++ src/components/InvalidHuntNamePage.js
@@ -6,7 +6,7 @@ function InvalidHuntNamePage(props) {
       <h1>We're sorry, the hunt name you chose is already taken</h1>
       <h2>Name your new treasure hunt:</h2>
       <input onChange={(e) => props.handleInputChange(e)} name={'newHuntName'} placeholder={"E.g. 'My favorite benches'"}></input>
-      <button onClick={() => props.handleSubmitNewHunt()}>Try again!</button>
+      <button onClick={() => props.handleSubmitNewHunt()}>Save Hunt!</button>
     </div>
   )
 }
