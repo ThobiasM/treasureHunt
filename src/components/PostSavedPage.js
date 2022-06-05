53730f847fc7f0c34604aae7bd8877b9668246f2
Christine Frances Betten <christinebetten@gmail.com>
2022-06-05 13:23:35 +0200

    Convert PostSavedPage to functional component

--- src/components/PostSavedPage.js
+++ src/components/PostSavedPage.js
@@ -1,43 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class PostSavedPage extends React.Component {
-  constructor(props) {
-    super(props);
+const PostSavedPage = ({addNewPost, handleInputChange, handleSubmitNewHunt}) => {
+  const [showFinishMessage, setShowFinishMessage] = useState(false);
 
-    this.state = {
-      showFinishMessage: false,
-    }
-  }
-
-  handleShowFinishMessage() {
-    this.setState({
-      showFinishMessage: true,
-    })
-  }
-
-  render() {
-    return(
-      <div className="post-saved-view">
-        <h2>Post saved!</h2>
-        {!this.state.showFinishMessage &&
-        <div className='post-added-btn-container'>
-          <button onClick={() => this.props.addNewPost()}>Add another post</button>
-          <button onClick={() => this.handleShowFinishMessage()}>Finish treasure hunt</button>
-        </div>
-        }
-  
-        {this.state.showFinishMessage &&
-        <div className='final-message-and-submit'>
-          <label htmlFor={'newFinalMessage'}>
-            Do you want to give the player a final message when they finish your treasure hunt?
-          </label>
-          <input onChange={(e) => this.props.handleInputChange(e)} id={'newFinalMessage'} name={'newFinalMessage'} maxLength={500} placeholder={"Max 500 characters"}></input>
-          <button onClick={() => this.props.handleSubmitNewHunt()}>Save treasure hunt</button>
-        </div>
-        }
+  return (
+    <div className="post-saved-view">
+      <h2>Post saved!</h2>
+      {!showFinishMessage &&
+      <div className='post-added-btn-container'>
+        <button onClick={() => addNewPost()}>Add another post</button>
+        <button onClick={() => setShowFinishMessage(true)}>Finish treasure hunt</button>
       </div>
-    )
-  }
+      }
+
+      {showFinishMessage &&
+      <div className='final-message-and-submit'>
+        <label htmlFor={'newFinalMessage'}>
+          Do you want to give the player a final message when they finish your treasure hunt?
+        </label>
+        <input onChange={(e) => handleInputChange(e)} id={'newFinalMessage'} name={'newFinalMessage'} maxLength={500} placeholder={"Max 500 characters"}></input>
+        <button onClick={() => handleSubmitNewHunt()}>Save treasure hunt</button>
+      </div>
+      }
+    </div>
+  )
 }
 
 export default PostSavedPage;
