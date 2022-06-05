0c718da767d7b743ded4fd5e085f3c87a4b39ee0
Christine Frances Betten <christinebetten@gmail.com>
2022-06-05 13:48:26 +0200

    Syntax update for FoundView

--- src/components/FoundView.js
+++ src/components/FoundView.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-function FoundView(props) {
+const FoundView = ({currentPostId, nextPost}) => {
     return (
         <div className="infobox-content">
             <div className="found-view">
-                <h2>You found post #{props.currentPostId - 1}!</h2>
-                <button onClick={() => props.nextPost()}>Next post</button>
+                <h2>You found post #{currentPostId - 1}!</h2>
+                <button onClick={() => nextPost()}>Next post</button>
             </div>
         </div>
     )
