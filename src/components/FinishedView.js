64a646874de76a03d8431bb5d5c8d91cab37ab47
Christine Frances Betten <christinebetten@gmail.com>
2022-06-05 13:56:00 +0200

    Remove comments in FinishedView

--- src/components/FinishedView.js
+++ src/components/FinishedView.js
@@ -12,16 +12,6 @@ const FinishedView = ({huntname, finalmessage, restartHunt}) => {
   )
 }
 
-// function FinishedView(props) {
-//   return <div className="infobox-content">
-//     <div className="finished-view">
-//       <h3>YOU DID IT, YOU FINISHED {props.huntname}!</h3>
-//       {props.finalmessage && <p>{props.finalmessage}</p>}
-//       <button onClick={() => props.restartHunt()}>Back to main menu</button>
-//     </div>
-//   </div>
-// };
-
 export default FinishedView;
 
 
