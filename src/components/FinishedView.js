bd58ae8ed1cd291c08973935aed657711d4d6d1f
Christine Frances Betten <christinebetten@gmail.com>
2022-06-05 13:55:25 +0200

    Syntax update for FinishedView

--- src/components/FinishedView.js
+++ src/components/FinishedView.js
@@ -1,14 +1,26 @@
 import React from "react";
 
-function FinishedView(props) {
-  return <div className="infobox-content">
-    <div className="finished-view">
-      <h3>YOU DID IT, YOU FINISHED {props.huntname}!</h3>
-      {props.finalmessage && <p>{props.finalmessage}</p>}
-      <button onClick={() => props.restartHunt()}>Back to main menu</button>
+const FinishedView = ({huntname, finalmessage, restartHunt}) => {
+  return (
+    <div className="infobox-content">
+      <div className="finished-view">
+        <h3>YOU DID IT, YOU FINISHED {huntname}!</h3>
+        {finalmessage && <p>{finalmessage}</p>}
+        <button onClick={() => restartHunt()}>Back to main menu</button>
+      </div>
     </div>
-  </div>
-};
+  )
+}
+
+// function FinishedView(props) {
+//   return <div className="infobox-content">
+//     <div className="finished-view">
+//       <h3>YOU DID IT, YOU FINISHED {props.huntname}!</h3>
+//       {props.finalmessage && <p>{props.finalmessage}</p>}
+//       <button onClick={() => props.restartHunt()}>Back to main menu</button>
+//     </div>
+//   </div>
+// };
 
 export default FinishedView;
 
