5a216f5c117f0e02c03650d9aedd9de97b313e2c
Christine Frances Betten <christinebetten@gmail.com>
2022-06-05 13:28:33 +0200

    Convert NewPostInfo to functional component

--- src/components/NewPostInfo.js
+++ src/components/NewPostInfo.js
@@ -1,24 +1,24 @@
 import React from "react";
 
-function NewPostInfo(props) {
+const NewPostInfo = ({handleInputChange, newPostRadius, handleSavePost}) => {
   return (
     <div className="new-post-info">
       <label>
         Post name:
-        <input onChange={(e) => props.handleInputChange(e)} name={'newPostName'} placeholder="E.g. 'Bakery'"></input>
+        <input onChange={(e) => handleInputChange(e)} name={'newPostName'} placeholder="E.g. 'Bakery'"></input>
       </label>
 
       <label>
         Hint:
-        <input onChange={(e) => props.handleInputChange(e)} name={'newHint'} maxLength={500} placeholder={"Max 500 characters"}></input>
+        <input onChange={(e) => handleInputChange(e)} name={'newHint'} maxLength={500} placeholder={"Max 500 characters"}></input>
       </label>
 
       <label>
-        Radius: <span id="demo">{props.newPostRadius}m</span>
-        <input onChange={(e) => props.handleInputChange(e)} name={"newPostRadius"} type="range" min="5" max="100" step="5" className="slider" id="myRange"/>
+        Radius: <span id="demo">{newPostRadius}m</span>
+        <input onChange={(e) => handleInputChange(e)} name={"newPostRadius"} type="range" min="5" max="100" step="5" className="slider" id="myRange"/>
       </label>
 
-      <button onClick={() => props.handleSavePost()}>Save post</button>
+      <button onClick={() => handleSavePost()}>Save post</button>
     </div>
   )
 }
