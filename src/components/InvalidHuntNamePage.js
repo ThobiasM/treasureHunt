5b82b29a3f8147cb425eb76cde340b0060c51c4c
Christine Frances Betten <christinebetten@gmail.com>
2022-06-05 13:43:49 +0200

    Syntax update for InvalidHuntNamePage

--- src/components/InvalidHuntNamePage.js
+++ src/components/InvalidHuntNamePage.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-function InvalidHuntNamePage(props) {
+const InvalidHuntNamePage = ({handleInputChange, handleSubmitNewHunt}) => {
   return(
     <div className={'name-hunt-view'}>
       <h2 className="invalid-hunt-name">Oops! The hunt name you chose is already taken - please pick a new name.</h2>
       <h3>Name your new treasure hunt:</h3>
-      <input onChange={(e) => props.handleInputChange(e)} name={'newHuntName'} placeholder={"E.g. 'My favorite benches'"}></input>
-      <button onClick={() => props.handleSubmitNewHunt()}>Save Hunt!</button>
+      <input onChange={(e) => handleInputChange(e)} name={'newHuntName'} placeholder={"E.g. 'My favorite benches'"}></input>
+      <button onClick={() => handleSubmitNewHunt()}>Save Hunt!</button>
     </div>
   )
 }
